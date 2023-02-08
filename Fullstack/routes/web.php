2506a061699b31f79ee757f49e240aff4bb96d47
George <georgemiranda.sk@gmail.com>
2023-02-07 22:37:43 -0300

    modified:   Fullstack/routes/web.php

--- Fullstack/routes/web.php
+++ Fullstack/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\IndexController;
 
 
-
-
+Route::get('/', [IndexController::class, 'index']);
+Route::get('/hello', [IndexController::class, 'show']);
