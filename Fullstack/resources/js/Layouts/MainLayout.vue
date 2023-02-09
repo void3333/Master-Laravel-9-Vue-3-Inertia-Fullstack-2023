ddeaac31a398ef9a4a122a79f2c849398f19805f
George <georgemiranda.sk@gmail.com>
2023-02-08 23:43:24 -0300

    new file:   Fullstack/app/Http/Middleware/HandleInertiaRequests.php

--- Fullstack/resources/js/Layouts/MainLayout.vue
+++ Fullstack/resources/js/Layouts/MainLayout.vue
@@ -1,14 +1,31 @@
 <template>
     <Link href="/">Main Page</Link>&nbsp;
     <Link href="/hello">Show Page</Link>
-    <div>The page with time: {{ timer }}</div>
-    <slot>Default Slot</slot>
-</template>
 
+    <div v-if="flashSuccess" class="success">
+      {{ flashSuccess }}
+    </div>
 
-<script setup>
-    import { Link } from '@inertiajs/vue3'
-    import { ref } from 'vue'
-    const timer = ref(0)
-    setInterval(() => timer.value++, 1000)
-</script>
+    <slot>Default</slot>
+  </template>
+
+  <script setup>
+  import { computed } from 'vue'
+  import { Link, usePage } from '@inertiajs/vue3'
+
+ // const page = usePage()
+  const flashSuccess = computed(
+    () => usePage().props.flash.success,
+  )
+  // import { ref } from 'vue'
+
+  // const timer = ref(0)
+  // setInterval(() => timer.value++, 1000)
+  </script>
+
+    <style scoped>
+      .success {
+        background-color: green;
+        color: white;
+      }
+    </style>
