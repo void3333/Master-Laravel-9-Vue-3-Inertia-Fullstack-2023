4a51dca17d618961af380aee76af534768e44e8d
George <georgemiranda.sk@gmail.com>
2023-02-09 20:01:20 -0300

    added progressbar added @apply

--- Fullstack/resources/js/Layouts/MainLayout.vue
+++ Fullstack/resources/js/Layouts/MainLayout.vue
@@ -3,13 +3,11 @@
         <div class="container mx-auto">
             <nav class="p-4 flex items-center justify-between">
                 <div class="text-lg font-medium">
-                    <Link :href="route('listing.index')">Listings</Link>
+                    <Link :href="route('listing.index')">LaraZillow</Link>
                 </div>
-                <div class="text-xl text-indigo-600 dark:text-indigo-300 font-bold text-center"  >
-                    <Link :href="route('listing.index')">Larazillow</Link>
-                </div>
+
                 <div>
-                    <Link :href="route('listing.create')" class="bg-indigo-600 hover:bg-indigo-500 text-white font-medium p-2 rounded-md">+ Create Listing</Link>
+                    <Link :href="route('listing.create')" class="btn-primary">+ New Listing</Link>
                 </div>
             </nav>
         </div>
