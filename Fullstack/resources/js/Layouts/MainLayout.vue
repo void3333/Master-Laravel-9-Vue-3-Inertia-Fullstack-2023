bc883a5be4976a6bd653a88af85f4441c19353bd
George <georgemiranda.sk@gmail.com>
2023-02-09 19:29:28 -0300

    added style and info was passed to the view

--- Fullstack/resources/js/Layouts/MainLayout.vue
+++ Fullstack/resources/js/Layouts/MainLayout.vue
@@ -1,12 +1,12 @@
 <template>
-    <header class="border-b border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-800 w-full">
+    <header class="border-b border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-900 w-full">
         <div class="container mx-auto">
             <nav class="p-4 flex items-center justify-between">
                 <div class="text-lg font-medium">
                     <Link :href="route('listing.index')">Listings</Link>
                 </div>
                 <div class="text-xl text-indigo-600 dark:text-indigo-300 font-bold text-center"  >
-                    <Link :href="('listing.index')">Larazillow</Link>
+                    <Link :href="route('listing.index')">Larazillow</Link>
                 </div>
                 <div>
                     <Link :href="route('listing.create')" class="bg-indigo-600 hover:bg-indigo-500 text-white font-medium p-2 rounded-md">+ Create Listing</Link>
--- Fullstack/resources/js/Pages/Listing/Index.vue
+++ Fullstack/resources/js/Pages/Listing/Index.vue
@@ -3,7 +3,9 @@
         <Box v-for="listing in listings" :key="listing.id">
             <div>
                 <Link :href="route('listing.show', {listing: listing.id})">
-                <ListingAddress :listing="listing" />
+                <Price :price="listing.price" class="text-2xl font-bold" />
+                <ListingSpace :listing="listing" class="text-lg"/>
+                <ListingAddress :listing="listing" class="text-gray-500"/>
             </Link>
         </div>
         <div>
@@ -20,6 +22,8 @@
   import {Link} from '@inertiajs/vue3'
   import ListingAddress from '@/Components/ListingAddress.vue'
   import Box from '@/Components/UI/Box.vue'
+  import ListingSpace from '@/Components/ListingSpace.vue'
+  import Price from '@/Components/Price.vue'
   defineProps({
     listings: Array,
   })
